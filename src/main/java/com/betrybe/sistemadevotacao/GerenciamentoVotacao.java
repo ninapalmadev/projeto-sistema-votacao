e23a089c24f92c1c1d5ff7162eeed1f3786103f0
Nina Palma <ninapalmadev@gmail.com>
2024-01-24 19:39:11 -0300

    Requisito 5: Implementa os métodos de cadastro da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -14,14 +14,30 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
    * Cadastra pessoa candidata.
    */
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      int numeroExiste = pessoaCandidata.getNumero();
+      if (numeroExiste == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(pessoaCandidata);
   }
 
   /**
    * Cadastra pessoa eleitora.
    */
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      String cpfExiste = pessoaEleitora.getCpf();
+      if (cpfExiste.equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(pessoaEleitora);
   }
 
   /**
