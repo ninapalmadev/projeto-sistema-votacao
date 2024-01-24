2adb3c278b13f62f214848b551f5f1e37509c623
Nina Palma <ninapalmadev@gmail.com>
2024-01-24 19:35:32 -0300

    Requisito 4 - Implementa classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,41 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
 /**
  * The type Gerenciamento Votação.
  */
-public class GerenciamentoVotacao {
 
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
+
+  /**
+   * Cadastra pessoa candidata.
+   */
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  /**
+   * Cadastra pessoa eleitora.
+   */
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  /**
+   * Votar.
+   */
+
+  public void votar(String cpfsComputados, int numeroPessoaCandidata) {
+
+  }
+
+  /**
+   * Mostrar Resultado.
+   */
+  public void mostrarResultado() {
+
+  }
 }
