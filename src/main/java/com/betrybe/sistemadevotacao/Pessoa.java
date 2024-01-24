0bcb63cd4c5f8fe403c546f29a7996c84646b9f8
Nina Palma <ninapalmadev@gmail.com>
2024-01-24 17:40:03 -0300

    Requisito 1 - Classe Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -3,6 +3,15 @@ package com.betrybe.sistemadevotacao;
 /**
  * the Type Pessoa.
 */
-public class Pessoa {
-  
+public abstract class Pessoa {
+
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
